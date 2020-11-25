2ace68645dbe24e25113245ae4ebacf0686c6f12
pome-ta <pome-ta@users.noreply.github.com>
2020-11-26 08:52:10 +0900

    1st

---
+++ run.py
@@ -0,0 +1,21 @@
+import pythonista.wkwebview as wkwebview
+import ui
+import pathlib
+
+uri = pathlib.Path('./main_index.html')
+
+
+
+class View(ui.View):
+  def __init__(self):
+    self.wv = wkwebview.WKWebView(swipe_navigation=True, flex='WH')
+    self.wv.load_url(str(uri))
+    self.add_subview(self.wv)
+
+  def will_close(self):
+    self.wv.clear_cache()
+
+
+_view = View()
+_view.present(style='fullscreen', orientations=['portrait'])
+
