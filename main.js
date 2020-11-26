5c686c3bc9b031c9faac79a6c0ef668840ee5edf
pome-ta <pome-ta@users.noreply.github.com>
2020-11-27 07:53:55 +0900

    div に逃がす

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 import CodeMirror from './src/codemirror.js'
 
 
-const myCodeMirror = CodeMirror(document.body, {
+const myCodeMirror = CodeMirror(document.getElementById('cm'), {
  value: "1loO0\nぴよよよ 😂\nさて、コードをどのように書くのか？🤔",
  lineNumbers: true,
  }
