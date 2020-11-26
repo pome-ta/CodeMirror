ef39c3032ea128d988c263ed97851860db9f977c
pome-ta <pome-ta@users.noreply.github.com>
2020-11-27 07:46:00 +0900

    main にマージする

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import CodeMirror from './src/codemirror.js'
 
 
 const myCodeMirror = CodeMirror(document.body, {
- value: "1loO0\nぴよよよ 😂\nコードをどのように書くのか？🤔",
+ value: "1loO0\nぴよよよ 😂\nさて、コードをどのように書くのか？🤔",
  lineNumbers: true,
  }
 );
