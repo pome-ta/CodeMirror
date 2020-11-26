9b1d7416e089c3219f3650ad94057da471039564
pome-ta <pome-ta@users.noreply.github.com>
2020-11-27 08:13:39 +0900

    テスト中

--- main.js
+++ main.js
@@ -3,6 +3,18 @@
 import CodeMirror from './src/codemirror.js'
 
 
+document.body.addEventListener('keydown',
+    event => {
+        if (event.key === 'v') {
+           alert("Ctrl+Vが押されました")
+        }
+    });
+
+
+let _e = document.createEvent("KeyboardEvent");
+_e.initKeyEvent("keydown", true, true, null, false, false, false, false, 65, 0);
+document.getElementById("dummy").dispatchEvent(_e);
+
 const myCodeMirror = CodeMirror(document.getElementById('cm'), {
  value: "1loO0\nぴよよよ 😂\nさて、コードをどのように書くのか？🤔",
  lineNumbers: true,
